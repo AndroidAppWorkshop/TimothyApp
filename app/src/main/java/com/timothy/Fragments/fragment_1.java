f92a3cd437efd706be18517be81d4b0daf758a31
Wilson <l7960261@gmail.com>
2015-05-31 01:02:19 +0800

    Use a Singleton Pattern via AndroidManifest.xml & BaseApplication Modify fragment_1 HttpRequest Modify strings.xml remove hello_world & activity_login

--- app/src/main/java/com/timothy/Fragments/fragment_1.java
+++ app/src/main/java/com/timothy/Fragments/fragment_1.java
@@ -1,18 +1,19 @@
 package com.timothy.Fragments;
 
 import org.json.JSONArray;
-import com.android.volley.RequestQueue;
+
+import com.android.volley.Request;
 import com.android.volley.Response;
-import com.android.volley.toolbox.Volley;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.timothy.Core.BaseApplication;
 import com.timothy.Tools.JsonHelper;
 import com.timothy.R;
-import com.timothy.Tools.ServerRequest;
 import com.timothy.Tools.UriResources;
 
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -23,126 +24,114 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class fragment_1  extends Fragment implements OnClickListener{
-	private RelativeLayout course , found , set;
-	private ImageView course_image  , settings_image ,found_image;
-    private TextView  course_text ,settings_text ,found_text ,txv;
+public class fragment_1 extends Fragment implements OnClickListener {
+    private RelativeLayout course, found, set;
+    private ImageView course_image, settings_image, found_image;
+    private TextView course_text, settings_text, found_text, txv;
     private int gray = 0xFF7597B3;
-	private int blue =0xFF0AB2FB;
+    private int blue = 0xFF0AB2FB;
     private View view;
     private LinearLayout me1;
-    RequestQueue RQueue;
-    String URL = UriResources.Test.OpenData1;
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		view = inflater.inflate(R.layout.fragment1, container, false);
 
-		initView(); 
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment1, container, false);
+        initView();
+        return view;
+    }
 
-		return view;
-	}
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        Log.e("DATA Report",  this.toString() + "Fragmen Has be Calllllllllllllllllll ! ");
-        RQueue = Volley.newRequestQueue(getActivity());
-
-        Response.Listener<JSONArray> listener = new Response.Listener<JSONArray>() {
-            @Override
-            public void onResponse(JSONArray jsonArray) {
-
-                txv.setText(JsonHelper.readLine(jsonArray));
-
-            }
-        };
-        new ServerRequest( RQueue , URL , listener).MgetRequest();
+        BaseApplication.getInstance(getActivity()).addToRequestQueue(
+                new JsonArrayRequest(
+                        Request.Method.GET,
+                        UriResources.Test.OpenData1,
+                        new Response.Listener<JSONArray>() {
+                            @Override
+                            public void onResponse(JSONArray jsonArray) {
+                                txv.setText(JsonHelper.readLine(jsonArray));
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError volleyError) {
+                            }
+                        }
+                )
+        );
     }
 
-    private void initView()
-	{			
-		me1=(LinearLayout)view.findViewById(R.id.me1);
-		course=(RelativeLayout)view.findViewById(R.id.course_layout);
-		found=(RelativeLayout)view.findViewById(R.id.found_layout);
-		set=(RelativeLayout)view.findViewById(R.id.setting_layout);
-		course_image = (ImageView)view.findViewById(R.id.course_image);  
-        found_image = (ImageView)view.findViewById(R.id.found_image);  
-        settings_image = (ImageView)view.findViewById(R.id.setting_image);  
-        course_text = (TextView)view.findViewById(R.id.course_text);  
-        found_text = (TextView)view.findViewById(R.id.found_text);  
-        settings_text = (TextView)view.findViewById(R.id.setting_text);  
-		course.setOnClickListener(this);
-		found.setOnClickListener(this);
-		set.setOnClickListener(this);
-		me1.getBackground().setAlpha(200);
-        txv=(TextView)view.findViewById(R.id.txv);
-	}
+    private void initView() {
+        me1 = (LinearLayout) view.findViewById(R.id.me1);
+        course = (RelativeLayout) view.findViewById(R.id.course_layout);
+        found = (RelativeLayout) view.findViewById(R.id.found_layout);
+        set = (RelativeLayout) view.findViewById(R.id.setting_layout);
+        course_image = (ImageView) view.findViewById(R.id.course_image);
+        found_image = (ImageView) view.findViewById(R.id.found_image);
+        settings_image = (ImageView) view.findViewById(R.id.setting_image);
+        course_text = (TextView) view.findViewById(R.id.course_text);
+        found_text = (TextView) view.findViewById(R.id.found_text);
+        settings_text = (TextView) view.findViewById(R.id.setting_text);
+        course.setOnClickListener(this);
+        found.setOnClickListener(this);
+        set.setOnClickListener(this);
+        me1.getBackground().setAlpha(200);
+        txv = (TextView) view.findViewById(R.id.txv);
+    }
 
-	@Override
-	public void onClick(View v) {
-		 switch (v.getId()) 
-		 {
-		 	case R.id.course_layout:  
-	            setTabSelection(0);
-	           Toast.makeText(getActivity(),"�ڬO��{��", Toast.LENGTH_SHORT).show();
-	            break;  
-	        case R.id.found_layout:  
-	            setTabSelection(1);
-	            Toast.makeText(getActivity(),"�ڬO�o�{", Toast.LENGTH_SHORT).show();
-	            break;  
-	        case R.id.setting_layout:  
-	            setTabSelection(2);
-	            Toast.makeText(getActivity(),"�ڬO�]�m", Toast.LENGTH_SHORT).show();
-	            break;  
-	      	  
-	        default:  
-	            break;  
-		 }
-		
-	}
-	
-	private void setTabSelection(int i) {
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.course_layout:
+                setTabSelection(0);
+                Toast.makeText(getActivity(), "�ڬO��{��", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.found_layout:
+                setTabSelection(1);
+                Toast.makeText(getActivity(), "�ڬO�o�{", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.setting_layout:
+                setTabSelection(2);
+                Toast.makeText(getActivity(), "�ڬO�]�m", Toast.LENGTH_SHORT).show();
+                break;
 
-        resetBtn();       
-        switch (i)  
-        {  
-        case 0:  
-           
-        	course_image.setImageResource(R.drawable.buttonbar_image_click);
-        	//course.setBackgroundColor(R.drawable.ic_tabbar_bg_click);
-        	course_text.setTextColor(blue);
-            break;  
-        case 1:  
-          
-        	found_image.setImageResource(R.drawable.buttonbar_image_click2);
-        	//found.setBackgroundColor(R.drawable.ic_tabbar_found_pressed);
-        	found_text.setTextColor(blue); 
-            break;  
-        case 2:  
-           
-        	settings_image.setImageResource(R.drawable.buttonbar_image_click3);
-        	//set.setBackgroundColor(R.drawable.ic_tabbar_settings_pressed);
-        	settings_text.setTextColor(blue);  
-            break; 
-            
-        default:  
-            break; 
-        }     
-		
-	}
-	private void resetBtn() {
-		course_image.setImageResource(R.drawable.buttonbar_image_create);
-        //course.setBackgroundColor(R.drawable.bottom_bar);  
-        course_text.setTextColor(gray);  
+            default:
+                break;
+        }
+
+    }
+
+    private void setTabSelection(int i) {
+
+        resetBtn();
+        switch (i) {
+            case 0:
+                course_image.setImageResource(R.drawable.buttonbar_image_click);
+                course_text.setTextColor(blue);
+                break;
+            case 1:
+                found_image.setImageResource(R.drawable.buttonbar_image_click2);
+                found_text.setTextColor(blue);
+                break;
+            case 2:
+                settings_image.setImageResource(R.drawable.buttonbar_image_click3);
+                settings_text.setTextColor(blue);
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    private void resetBtn() {
+        course_image.setImageResource(R.drawable.buttonbar_image_create);
+        course_text.setTextColor(gray);
         found_image.setImageResource(R.drawable.buttonbar_image_create2);
-       // found.setBackgroundColor(R.drawable.bottom_bar);  
-        found_text.setTextColor(gray);  
+        found_text.setTextColor(gray);
         settings_image.setImageResource(R.drawable.buttonbar_image_create3);
-        //set.setBackgroundColor(R.drawable.bottom_bar);  
-        settings_text.setTextColor(gray);  
-	}
-
-	
-	
+        settings_text.setTextColor(gray);
+    }
 }
