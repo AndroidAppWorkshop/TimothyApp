8e8788396d454f181c532412265a43f23a5b049c
Wilson <l7960261@gmail.com>
2015-06-06 17:19:48 +0800

    Update Http Request

--- app/src/main/java/com/timothy/Activitys/MenuListActivity.java
+++ app/src/main/java/com/timothy/Activitys/MenuListActivity.java
@@ -15,6 +15,7 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -22,6 +23,7 @@ import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.NetworkImageView;
 import com.android.volley.toolbox.Volley;
+import com.timothy.Core.BaseApplication;
 import com.timothy.Menu.Cart;
 import com.timothy.Menu.Category;
 import com.timothy.Cache.LruBitmapCache;
@@ -29,6 +31,7 @@ import com.timothy.Cache.LruBitmapCache;
 import com.timothy.Menu.Product;
 import com.timothy.Menu.ProductRepository;
 import com.timothy.R;
+
 import library.timothy.Resources.UriResources;
 
 import org.json.JSONArray;
@@ -38,16 +41,16 @@ import java.util.List;
 
 public class MenuListActivity extends Activity implements View.OnClickListener {
 
-    private ProductRepository productRepository = new ProductRepository();//做分類
-    private final Cart cart =new Cart();//new 購物車
-    private int total=0;
+    private ProductRepository productRepository = new ProductRepository();
+    private final Cart cart = new Cart();
+    private int total = 0;
     private TextView textViewPriceSum;
     private TextView textViewTotal;
     private ProgressBar progressBar;
     private Button commit;
     private ListView listView;
-    RequestQueue mQueue;
-     @Override
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menulayout);
@@ -55,28 +58,32 @@ public class MenuListActivity extends Activity implements View.OnClickListener {
         textViewPriceSum = (TextView) findViewById(R.id.textViewTotalPrice);
         textViewTotal = (TextView) findViewById(R.id.textViewTotal);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        commit = (Button)findViewById(R.id.button1);
+        commit = (Button) findViewById(R.id.button1);
         commit.setOnClickListener(this);
-        mQueue = Volley.newRequestQueue(this);
 
         progressBar.setVisibility(View.VISIBLE);
-        JsonArrayRequest request =new JsonArrayRequest(UriResources.Server.Product,new Response.Listener<JSONArray>() {
-            @Override
-            public void onResponse(JSONArray jsonArray) {
-                progressBar.setVisibility(View.GONE);
-                productRepository.refreshdata(jsonArray);
-                renderlistview();
-            }
-        },new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError volleyError)
-            {
-                progressBar.setVisibility(View.GONE);
-                Toast.makeText(MenuListActivity.this,volleyError.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });
 
-         mQueue.add(request);
+        BaseApplication.getInstance().addToRequestQueue(
+                new JsonArrayRequest(
+                        Request.Method.GET,
+                        UriResources.Server.Product,
+                        new Response.Listener<JSONArray>() {
+                            @Override
+                            public void onResponse(JSONArray jsonArray) {
+                                progressBar.setVisibility(View.GONE);
+                                productRepository.refreshdata(jsonArray);
+                                renderlistview();
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError volleyError) {
+                                progressBar.setVisibility(View.GONE);
+                                Toast.makeText(MenuListActivity.this, volleyError.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                )
+        );
     }
 
     private void renderlistview() {
@@ -91,8 +98,8 @@ public class MenuListActivity extends Activity implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
-        Intent it = new Intent(this , CartActivity.class );
-        it.putExtra("Data",cart);
+        Intent it = new Intent(this, CartActivity.class);
+        it.putExtra("Data", cart);
         startActivity(it);
     }
 
@@ -153,76 +160,70 @@ public class MenuListActivity extends Activity implements View.OnClickListener {
         public ProductPagerAdapter(List<Product> products) {
             this.products = products;
         }
-        NetworkImageView image ;
-        RequestQueue queue ;
+
+        NetworkImageView image;
+        RequestQueue queue;
         LruBitmapCache cache;
         ImageLoader ImageLoader;
+
         @Override
         public Object instantiateItem(ViewGroup container, int pagerPosition) {
             cache = new LruBitmapCache();
             queue = Volley.newRequestQueue(getApplicationContext());
-            ImageLoader = new ImageLoader(queue , cache);
+            ImageLoader = new ImageLoader(queue, cache);
             View inflate = getLayoutInflater().inflate(R.layout.pageritem_container, null);
             LinearLayout pagerContainer = (LinearLayout) inflate.findViewById(R.id.pagerContainer);
 
-                int start = 3 * pagerPosition + 0;// 012,345,678
-                int end = 3 * pagerPosition + 2;
-                int[] pagerItemProductViewIds = {R.id.pagerItem1, R.id.pagerItem2, R.id.pagerItem3};
-                for (int i = start; i <= end; i++) {
-                    View productView = pagerContainer.findViewById(pagerItemProductViewIds[i - 3 * pagerPosition]);
+            int start = 3 * pagerPosition + 0;
+            int end = 3 * pagerPosition + 2;
+            int[] pagerItemProductViewIds = {R.id.pagerItem1, R.id.pagerItem2, R.id.pagerItem3};
+            for (int i = start; i <= end; i++) {
+                View productView = pagerContainer.findViewById(pagerItemProductViewIds[i - 3 * pagerPosition]);
 
-                    Product product = null;
-                    try {
-                        product = products.get(i);
-                    } catch (Exception e) {
-                        productView.setVisibility(View.INVISIBLE);
-                        continue;
-                    }
+                Product product = null;
+                try {
+                    product = products.get(i);
+                } catch (Exception e) {
+                    productView.setVisibility(View.INVISIBLE);
+                    continue;
+                }
 
-                    TextView textViewProductName = (TextView) productView.findViewById(R.id.textViewProductName);
-                    textViewProductName.setText(product.getName());
+                TextView textViewProductName = (TextView) productView.findViewById(R.id.textViewProductName);
+                textViewProductName.setText(product.getName());
 
-                    TextView textViewProductPrice = (TextView) productView.findViewById(R.id.textViewProductPrice);
-                    final int price = product.getPrice();
-                    textViewProductPrice.setText(String.valueOf(price));
+                TextView textViewProductPrice = (TextView) productView.findViewById(R.id.textViewProductPrice);
+                final int price = product.getPrice();
+                textViewProductPrice.setText(String.valueOf(price));
 
-                    final TextView textViewProductCount = (TextView) productView.findViewById(R.id.textViewProductCount);
-                    final String productId = product.getId();
-
+                final TextView textViewProductCount = (TextView) productView.findViewById(R.id.textViewProductCount);
+                final String productId = product.getId();
 
                 textViewProductCount.setText(String.valueOf(cart.getProductCountInCart(productId)));
 
-//                image = (NetworkImageView) productView.findViewById(R.id.image);
-//                image.setImageUrl(productVo.getimage() , ImageLoader );
-
                 Button btnAdd = (Button) productView.findViewById(R.id.btnAdd);
 
-                  btnAdd.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            cart.addToCart(productId, 1);
-                            textViewProductCount.setText(String.valueOf(cart.getProductCountInCart(productId)));
-                            textViewPriceSum.setText(String.valueOf(cart.calculateSumPrice()));
-                            textViewTotal.setText(String.valueOf(cart.calculateSumCount()));
-                        }
-                    });
+                btnAdd.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        cart.addToCart(productId, 1);
+                        textViewProductCount.setText(String.valueOf(cart.getProductCountInCart(productId)));
+                        textViewPriceSum.setText(String.valueOf(cart.calculateSumPrice()));
+                        textViewTotal.setText(String.valueOf(cart.calculateSumCount()));
+                    }
+                });
 
-                    Button btnDecrease = (Button) productView.findViewById(R.id.btnDecrease);
-                    btnDecrease.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
+                Button btnDecrease = (Button) productView.findViewById(R.id.btnDecrease);
+                btnDecrease.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
 
-                            cart.addToCart(productId, -1);
-                            textViewProductCount.setText(String.valueOf(cart.getProductCountInCart(productId)));
-                            textViewPriceSum.setText(String.valueOf(cart.calculateSumPrice()));
-                            textViewTotal.setText(String.valueOf(cart.calculateSumCount()));
-                        }
-                    });
-
-
-                }
-
-
+                        cart.addToCart(productId, -1);
+                        textViewProductCount.setText(String.valueOf(cart.getProductCountInCart(productId)));
+                        textViewPriceSum.setText(String.valueOf(cart.calculateSumPrice()));
+                        textViewTotal.setText(String.valueOf(cart.calculateSumCount()));
+                    }
+                });
+            }
             container.addView(inflate);
             return inflate;
         }
@@ -241,8 +242,6 @@ public class MenuListActivity extends Activity implements View.OnClickListener {
         public void destroyItem(ViewGroup container, int position, Object object) {
             container.removeView((View) object);
         }
-
     }
-
 
 }
