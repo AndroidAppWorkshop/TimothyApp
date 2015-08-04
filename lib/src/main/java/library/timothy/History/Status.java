3d0470320c61667e4435b7523bc52a6401078c55
uni <s1012681@pu.edu.tw>
2015-08-04 21:51:01 +0800

    fix history

---
+++ lib/src/main/java/library/timothy/History/Status.java
@@ -0,0 +1,20 @@
+package library.timothy.History;
+
+import library.timothy.Resources.StringResources;
+
+/**
+ * Created by h94u04 on 2015/7/12.
+ */
+public class Status extends Product{
+    private int totalprice =0;
+    private int discount =0;
+    public Status(int totalprice , int discount) {
+        this.totalprice = totalprice;
+        this.discount = discount;
+    }
+    @Override
+    public String getName() {
+        return StringResources.Text.AllPrice +totalprice + StringResources.Text.Discount + discount;
+    }
+
+}
